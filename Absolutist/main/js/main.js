cbba8d48a42cb7ea870bc00828b780824e01a644
HypernateOfficialProgramming <contreras.kratoskane@gmail.com>
2026-04-27 01:37:39 +0400

    Update main.js

--- Absolutist/main/js/main.js
+++ Absolutist/main/js/main.js
@@ -6,5 +6,5 @@ ctx.fillRect(0, 0, spacetime.width, spacetime.height);
 
 ctx.fillStyle = "white";
 ctx.beginPath();
-ctx.arc(250, 150, 50, 0, Math.PI * 2);
+ctx.arc(spacetime.width/2, spacetime.height/2, 10, 0, Math.PI * 2);
 ctx.fill();
